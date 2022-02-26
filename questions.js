bf4e5391c6d582ab8f87f624e02d00afcdbbc739
viveksharma2911 <92039146+Vivek29112001@users.noreply.github.com>
2022-02-26 12:07:08 +0530

    quiz question based on web_development

---
+++ questions.js
@@ -0,0 +1,179 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "Frames from one LAN can be transmitted to another LAN via the device",
+    answer: "Bridge",
+    options: [
+      "Modem",
+      "Repeater",
+      "Bridge",
+      "Router"
+    ]
+  },
+  {
+    numb:7 ,
+    question: "Which of the following condition is used to transmit two packets over a medium at the same time?",
+    answer: "Collision",
+    options: [
+      "Collision",
+      "Repeater",
+      "Synchronous",
+      "None of the above"
+    ]
+  },
+  {
+    numb:8 ,
+    question: "How many digits of the DNIC (Data Network Identification Code) identify the country?",
+    answer: "first three",
+    options: [
+      "first five",
+      "first four",
+      "first three",
+      "None of the above"
+    ]
+  },
+  
+  {
+    numb:9 ,
+    question: "Format flags may be combined using",
+    answer: "the bitwise OR operator(|)",
+    options: [
+      "the bitwise OR operator(|)",
+      "the logical OR operator(||)",
+      "the bitwise AND operator(&)",
+      "None of the above"
+    ]
+  },
+  {
+    numb:10 ,
+    question: "You can use C++ as a procedural, as well as an object-oriented, language",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "not sure",
+      "None of the above",
+      
+    ]
+  },
+  {
+    numb:11 ,
+    question: "The brain of any computer system is",
+    answer: "CPU",
+    options: [
+      "CPU",
+      "ALU",
+      "Memory",
+      "Control Unit",
+      
+    ]
+  },
+
+  {
+    numb:12 ,
+    question: "Which part interprets program instructions and initiate control operations.",
+    answer: "Control Unit",
+    options: [
+      "CPU",
+      "ALU",
+      "Memory",
+      "Control Unit",
+      
+    ]
+  },
+  {
+    numb:13 ,
+    question: "The binary system uses powers of",
+    answer: "2",
+    options: [
+      "8",
+      "2",
+      "10",
+      "16",
+      
+    ]
+  },
+  {
+    numb:14 ,
+    question: "Which of the following refers to something that can be identified in the users' work environment, something that the users want to track?",
+    answer: "Entity",
+    options: [
+      "Entity",
+      "Attribute",
+      "Identifier",
+      "Relationship",
+      
+    ]
+  },
+  {
+    numb:15 ,
+    question: "A recursive relationship is a relationship between an entity and ________ .",
+    answer: "itself",
+    options: [
+      "itself",
+      "Attribute",
+      "Identifier",
+      "a subtype entity",
+      
+    ]
+  },
+];
